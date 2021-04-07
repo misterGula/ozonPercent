49517b8a1d6413ffcebb6f5c3445f8b386ff14a8
Arti Gula <mister.gula@bk.ru>
2021-04-07 13:53:08 +0300

    Добавлен функционал прогнозирования зарплаты

--- main.js
+++ main.js
@@ -4,8 +4,7 @@
 //Tabs
 let tab = document.querySelectorAll('.menu-tabs'),
     menu = document.querySelector('.menu'),
-    tabContent = document.querySelectorAll('.tabcontent'),
-    outputData = document.querySelector('.outputData');
+    tabContent = document.querySelectorAll('.tabcontent');
 
 function hideTabContent(a) {
     for (let i = a; i < tab.length; i++) {
@@ -18,9 +17,7 @@ hideTabContent(1);
 function showTabContent(b) {
     if (tabContent[b].classList.contains('hide')) {
         tabContent[b].classList.remove('hide');
-
         tabContent[b].classList.add('show');
-
     };
 };
 
@@ -41,7 +38,7 @@ menu.addEventListener('click', function (event) {
 // application data
 
 let appData = {
-    factDay: 15,
+    factDay: 0,
     errors: 0,
     allPercent: 0,
     wantPercent: 0,
@@ -88,7 +85,7 @@ function calcTaxDeduction(value) {
 // application input value
 
 let start = document.getElementById('start'),
-    factDay = document.querySelector('.factDay-item'),
+    factDay = document.getElementsByClassName('factDay-item'),
     allPercent = document.querySelector('.allPercent-item'),
     errors = document.querySelector('.error-item');
 
@@ -98,14 +95,15 @@ let prognozWage = document.getElementById('prognozWage'),
 
 // application output value
 
-let wageValue = document.querySelector('.wage-value'),
-    factWageValue = document.querySelector('.factWage-value'),
-    bonusValue = document.querySelector('.bonus-value'),
-    factBonusValue = document.querySelector('.factBonus-value'),
-    dayWage = document.querySelector('.dayWage-value'),
-    allWage = document.querySelector('.allWage-value'),
-    factWage = document.querySelector('.factWages-value'),
-    middlePercent = document.querySelector('.middlePercent-value');
+let wageValue = document.getElementsByClassName('wage-value'),
+    factWageValue = document.getElementsByClassName('factWage-value'),
+    bonusValue = document.getElementsByClassName('bonus-value'),
+    factBonusValue = document.getElementsByClassName('factBonus-value'),
+    dayWage = document.getElementsByClassName('dayWage-value'),
+    allWage = document.getElementsByClassName('allWage-value'),
+    factWage = document.getElementsByClassName('factWages-value'),
+    middlePercent = document.getElementsByClassName('middlePercent-value');
+
 
 
 // application work
@@ -113,28 +111,47 @@ let wageValue = document.querySelector('.wage-value'),
 start.addEventListener('click', function () {
 
     appData.getMaxDay();
-    appData.factDay = factDay.value;
+    appData.factDay = factDay[0].value;
     appData.allPercent = allPercent.value;
     appData.errors = errors.value;
 
     let a = +(appData.monthWage * (appData.factDay / appData.maxDay)).toFixed(2);
-    wageValue.textContent = a;
-    factWageValue.textContent = calcTaxDeduction(a);
+    wageValue[0].textContent = a;
+    factWageValue[0].textContent = calcTaxDeduction(a);
     let b = +(appData.calcDayBonus(appData.allPercent, appData.factDay) * appData.factDay);
-    bonusValue.textContent = b;
-    factBonusValue.textContent = calcTaxDeduction(b);
-    allWage.textContent = a + b;
-    middlePercent.textContent = (appData.allPercent / appData.factDay).toFixed(2);
+    bonusValue[0].textContent = b;
+    factBonusValue[0].textContent = calcTaxDeduction(b);
+    allWage[0].textContent = a + b;
+    middlePercent[0].textContent = (appData.allPercent / appData.factDay).toFixed(2);
     let daySum = (a + b) / appData.factDay;
-    dayWage.textContent = calcTaxDeduction(daySum);
+    dayWage[0].textContent = calcTaxDeduction(daySum);
     let d = (daySum * appData.factDay) - appData.errors * 250;
-    factWage.textContent = calcTaxDeduction(d);
+    factWage[0].textContent = calcTaxDeduction(d);
 
 });
 
 prognozWage.addEventListener('click', function () {
 
-    
+    appData.wantPercent = wantPercent.value;
+    appData.getMaxDay();
+    appData.factDay = factDay[1].value;
+    if(appData.factDay == ''){appData.factDay = 15 };
+
+    let a = +(appData.monthWage * (appData.factDay / appData.maxDay)).toFixed(2);
+    wageValue[1].textContent = a;
+    factWageValue[1].textContent = calcTaxDeduction(a);
+    let x = appData.wantPercent*appData.factDay;
+    let b = +(appData.calcDayBonus(x, appData.factDay) * appData.factDay);
+    bonusValue[1].textContent = b;
+    factBonusValue[1].textContent = calcTaxDeduction(b);
+    allWage[1].textContent = a + b;
+    middlePercent[1].textContent = appData.wantPercent;
+    let daySum = (a + b) / appData.factDay;
+    dayWage[1].textContent = calcTaxDeduction(daySum);
+    let d = (daySum * appData.factDay) - appData.errors * 250;
+    factWage[1].textContent = calcTaxDeduction(d);
+
+
 
 });
 
