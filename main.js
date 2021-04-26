87ff8a14b9371485ffc8ca9eb0436c7cd0b29c41
Arti Gula <mister.gula@bk.ru>
2021-04-26 22:27:00 +0300

    Сделан график на текущий месяц

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ function createCalendar(id, year, month) {
     
     let d = new Date(year, month);
 
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+    let table = '<table id = "graph"><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
 
 
     for (let i = 0; i < getDay(d); i++) {
@@ -223,7 +223,38 @@ let dateMonth = new Date().getMonth();
 
 createCalendar("calendar", dateYear, dateMonth);
 
+let td = document.querySelectorAll('td');
+td.forEach(td => {
 
+            let cell = +td.innerHTML;
+            cell = cell - 1;
+            if(cell >= 0){
+            let x = howManyDay(0,cell);
+                if(x % 4 == 0){
+                td.classList.add('active');
+                } 
+            let y = howManyDay(1,cell);
+                if(y % 4 == 0){
+                td.classList.add('active');
+                }
+            }                     
+        });
 
+function howManyDay(smena,value) {
+    const pointDateOne = new Date(2021,3,0).getTime();
+    let dayOne = 1000 * 60 * 60 * 24;
+    let dayOnePoint = Math.floor(pointDateOne / dayOne);  
+    let now = new Date();
+    let whatDay = new Date(now.getFullYear(),now.getMonth(),value).getTime();
+    let whatDay1 = Math.floor(whatDay / dayOne);
+    if (smena == 1){
+        dayOnePoint = dayOnePoint + 1;
+        return (whatDay1 - dayOnePoint);
+    } else{
+        return (whatDay1 - dayOnePoint);
+    }
+ }; 
+ 
+ 
 
 
